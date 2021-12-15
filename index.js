6e200ce736068362015765a8bb458927c9428d8e
Mudhi24 <94287604+Mudhi24@users.noreply.github.com>
2021-12-15 23:49:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 var FastText = require('node-fasttext');
 const cors = require('cors');
 
 let config = { 
   dim: 100,
-  input: "train.txt",
+  input: "index.html",
   output: "model"
 }
 
@@ -33,7 +33,8 @@ app.get('/fasttext/', function(req, res) {
 
 function getFastTextResults(statement) {
 	//predict returns an array with the input and predictions for best cateogires
-	FastText.predict(
+	let result=null;
+  FastText.predict(
 		"model.bin", 3,
 		[statement],
 		function (success, error) {
@@ -42,11 +43,16 @@ function getFastTextResults(statement) {
 			console.log(error)
 			return;
 		  }
-		  console.log(success)
+      result=success;
 		})
-	return "success!";
+	return result;
 }
-
-app.listen(8000, () => {
+function addText(){
+  value1 = document.getElementById("getValue1").value
+  document.getElementById('putText').textContent = value1
+}
+const e1= document.getEleementById("button1");
+e1.addEventListener("click",addText)
+app.listen(80001, () => {
   console.log('Listening on port 8000!')
 });
